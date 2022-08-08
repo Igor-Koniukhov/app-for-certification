8db229fcaf67f15e597d746362c186eea673df91
igor Koniukhov <ikoniukhov@gmail.com>
2022-08-08 21:23:30 +0300

    added BroserRoter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'regenerator-runtime/runtime';
-import React, { useEffect} from 'react';
-import { Route, Switch} from "react-router-dom";
+import React, {useEffect} from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import './global.css'
 import IntroContent from "./components/IntroContent";
 import Sociology from "./pages/subjects/Sociology";
@@ -12,6 +12,7 @@ import Layout from "./components/layout/Layout";
 import Certificate from "./pages/Certificate";
 import Home from "./pages/Home"
 import NFTCollections from "./pages/NFTCollections";
+
 let pageIsLoad = true;
 
 export default function App() {
@@ -27,43 +28,46 @@ export default function App() {
     return (
         <Layout>
             <Switch>
-                {!isSignedIn && <IntroContent/>}
-                {isSignedIn &&
-                    <Route path='/' exact>
-                        <Home/>
-                    </Route>}
-                <Route path='/chemistry'>
-                    <Chemistry
-                        isLoad={pageIsLoad}
-                    />
-                </Route>
-                <Route path='/sociology'>
-                    <Sociology
-                        isLoad={pageIsLoad}
-                    />
-                </Route>
-                <Route path='/physic'>
-                    <Physic
-                        isLoad={pageIsLoad}
-                    />
-                </Route>
-                <Route path='/microbiology'>
-                    <Microbiology
-                        isLoad={pageIsLoad}
-                    />
-                </Route>
-                }
+                <BrowserRouter basename="/app-for-certification">
+                    {!isSignedIn && <IntroContent/>}
+                    {isSignedIn &&
+                        <Route path='/' exact>
+                            <Home/>
+                        </Route>}
+                    <Route path='/chemistry'>
+                        <Chemistry
+                            isLoad={pageIsLoad}
+                        />
+                    </Route>
+                    <Route path='/sociology'>
+                        <Sociology
+                            isLoad={pageIsLoad}
+                        />
+                    </Route>
+                    <Route path='/physic'>
+                        <Physic
+                            isLoad={pageIsLoad}
+                        />
+                    </Route>
+                    <Route path='/microbiology'>
+                        <Microbiology
+                            isLoad={pageIsLoad}
+                        />
+                    </Route>
+                    }
 
-                <Route path='/results'>
-                    <Results/>
-                </Route>
-                <Route path='/certificate'>
-                    <Certificate/>
-                </Route>
-                <Route path='/nft-collections'>
-                    <NFTCollections/>
-                </Route>
+                    <Route path='/results'>
+                        <Results/>
+                    </Route>
+                    <Route path='/certificate'>
+                        <Certificate/>
+                    </Route>
+                    <Route path='/nft-collections'>
+                        <NFTCollections/>
+                    </Route>
+                </BrowserRouter>
             </Switch>
+
         </Layout>
     )
 }
