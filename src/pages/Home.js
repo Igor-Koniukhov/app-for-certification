73b1b1065185dfca452170d966e25a8ef71f56ad
igor Koniukhov <ikoniukhov@gmail.com>
2022-08-09 10:06:10 +0300

    changed configs - leave only for testnet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useEffect, useState} from "react";
 
 import "./Home.module.css";
 import ChemistryImage from "../img/subject/IUPAC-feature-image.png";
@@ -6,8 +6,25 @@ import PhysicImage from "../img/subject/physic.png";
 import SociologyImage from "../img/subject/sociology.png";
 import MicroBiologyImage from "../img/subject/microbiology.png"
 import SubjectItemButton from "../components/UI/SubjectItemButton";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const Home = () => {
+    const {new_default_data, get_status_init} = window.contract;
+    const [stateSpinner, setStateSpinner] = useState(false);
+    const {
+        set_subjects,
+    } = window.contract;
+    const [stateIsInit, setStateIsInit]=useState(false);
+
+    useEffect(()=>{
+        const isInit = async () =>{
+            await get_status_init({}).then((data)=>{
+                    setStateIsInit(data)
+                }
+            )
+        }
+        isInit();
+    }, [stateIsInit])
 
     const subjectRange = [
         {
@@ -28,25 +45,54 @@ const Home = () => {
         }
     ]
 
+    const initContractHandler = () => {
+        setStateSpinner(true)
+        const initContract = async () => {
+            await new_default_data({owner_id: window.accountId}).then((data) => {
+                if (data !== undefined || data !== null) {
+                    setStateSpinner(false)
+                    setStateIsInit(true)
+                }
+            }).then(()=>{
+                const setSubjects = async () => {
+                    await set_subjects({})
+                }
+                setSubjects();
+            })
+        }
+        initContract();
+    }
 
     return (
         <Fragment>
             <div className="container text-center pt-5 ">
                 <h1>Welcome to Examinator</h1>
-                <Fragment>
-                    <p>Choose your subject and start exam</p>
-                    <div className="row justify-content-center">
-                        {subjectRange.map((sbj, index) =>
-                            <SubjectItemButton
-                                key={index}
-                                image={sbj.img}
-                                path={sbj.path}
-                            />
-                        )}
-                    </div>
-                </Fragment>
+                { stateIsInit &&
+                    <Fragment>
+                        <p>Choose your subject and start exam</p>
+                        <div className="row justify-content-center">
+                            {subjectRange.map((sbj, index) =>
+                                <SubjectItemButton
+                                    key={index}
+                                    image={sbj.img}
+                                    path={sbj.path}
+                                />
+                            )}
+                        </div>
+                    </Fragment>}
+                {!stateIsInit &&
+                    <button className="btn btn-danger mt-4" onClick={initContractHandler}>Init contract</button>
+                }
 
             </div>
+            {stateSpinner &&
+                (
+                    <div className='backdrop'>
+                        <LoadingSpinner/>
+                    </div>
+                )
+            }
+
         </Fragment>
     )
 
