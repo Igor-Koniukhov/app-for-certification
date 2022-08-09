0f36e0911502ab212aab63ccf66961ee725e5c5e
igor Koniukhov <ikoniukhov@gmail.com>
2022-08-09 08:05:21 +0300

    changed testnet

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME ||'app-for-cert.i-koniukhov.testnet'
+const CONTRACT_NAME = process.env.CONTRACT_NAME ||'i-koniukhov.testnet'
 
 function getConfig(env) {
   switch (env) {
